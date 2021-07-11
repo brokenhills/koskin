f8bd423e3c3caf7bb30f5240e75def860739b2a9
brokenhills <boneyard@yandex.ru>
2021-07-11 17:09:34 +0300

    Added api and data services. Added writings and gallery components.

---
+++ front/src/app/gallery/gallery.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { GalleryItem } from '../models/galleryItem';
+
+@Component({
+  selector: 'app-gallery',
+  templateUrl: './gallery.component.html',
+  styleUrls: ['./gallery.component.css']
+})
+export class GalleryComponent implements OnInit {
+
+  gallery: GalleryItem[] = [];
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+    this.dataService.getGallery()
+    .subscribe(gallery => {
+      this.gallery = gallery;
+    });
+  }
+
+}
---
+++ front/src/app/writing/writing.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Writing } from 'src/app/models/writing';
+
+@Component({
+  selector: 'app-writing',
+  templateUrl: './writing.component.html',
+  styleUrls: ['./writing.component.css']
+})
+export class WritingComponent implements OnInit {
+
+  @Input()
+  writing: Writing;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
